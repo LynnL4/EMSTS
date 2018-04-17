8cd6281f6b525a747c8e28a051de8370d60cd4c5
turmary <turmary@126.com>
2018-04-17 10:05:38 +0000

    Add: i2c adc driver based on MRAA library

--- modules/adc/mraa_i2c_adc121c021.py
+++ modules/adc/mraa_i2c_adc121c021.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Author: turmary <turmary@126.com>
 # Author: Baozhu Zuo <zuobaozhu@gmail.com>
 # Copyright (c) 2018 Seeed Corporation.
 #
@@ -23,7 +24,38 @@
 from kernel import core
 import time
 import os
-from upm import pyupm_adc121c021 as upmAdc121c021
+import mraa
+
+
+ADDR_ADC121 = 0x50
+REG_ADDR_RESULT = 0x00
+REG_ADDR_ALERT  = 0x01
+REG_ADDR_CONFIG = 0x02
+REG_ADDR_LIMITL = 0x03
+REG_ADDR_LIMITH = 0x04
+REG_ADDR_HYST   = 0x05
+REG_ADDR_CONVL  = 0x06
+REG_ADDR_CONVH  = 0x07
+
+V_REF = 3.095
+ 
+class ADC121C021:
+    def __init__(self, busid, addr):
+        self.bus = mraa.I2c(busid)
+        self.bus.address(addr)
+
+        self.bus.writeReg(REG_ADDR_CONFIG, 0x20)
+ 
+    def value(self):
+        "Read ADC data 0-4095."
+        data_list = self.bus.readBytesReg(REG_ADDR_RESULT, 2)
+        # print('data list', data_list)
+        data = ((data_list[0] & 0x0f) << 8 | data_list[1]) & 0xfff
+        return data
+
+    def valueToVolts(self, v):
+        return v * V_REF * 2 / 4096
+ 
 
 class subcore(core.interface):
     def __init__(self,parameters,platform,debug):
@@ -35,7 +67,7 @@ class subcore(core.interface):
             "description": self.parameters["description"],
             "result": "ok"
         }
-        self.myAnalogDigitalConv = upmAdc121c021.ADC121C021(self.parameters["busID"], 0x50)
+        self.myAnalogDigitalConv = ADC121C021(self.parameters["busID"], ADDR_ADC121)
 
     def do_test(self):
         for a in range(10):
@@ -49,4 +81,13 @@ class subcore(core.interface):
 
         return self.ret
 
+
+if __name__ == '__main__':
+	d = {
+		"volts": 3.0,
+		"bias" : 0.2
+	}
+	a = ADC121C021(0, ADDR_ADC121)
+	print(a.value())
+	print("voltage = %f\n" % a.valueToVolts(a.value()))
 
