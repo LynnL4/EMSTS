869f67c30646f1b2e4f410fa0151b4223930ecb5
turmary <turmary@126.com>
2019-11-01 08:04:53 +0000

    Add: blockdev-aging install script

---
+++ scripts/blk_aging/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root (use sudo)" 1>&2
+	exit 1
+fi
+
+cp blockdev_aging       /usr/local/bin/
+[ -f blockdev_aging.local ] || {
+	echo "Prepare a blockdev_aging.local script" 1>&2
+	exit 2
+}
+cp blockdev_aging.local /usr/local/bin/
+cp blockdev-aging.service /etc/systemd/system/
+systemctl unmask blockdev-aging.service
+systemctl enable blockdev-aging.service
+systemctl start  blockdev-aging.service
+
+exit 0
+
